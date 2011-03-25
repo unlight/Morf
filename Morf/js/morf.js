d1179e1dfab4f003e4afb90310467487bbd7e50c
Ronnie <unlights@gmail.com>
2011-03-25 13:15:23 +0300

    Final fixes

--- Morf/js/morf.js
+++ Morf/js/morf.js
@@ -3,44 +3,41 @@ $(document).ready(function(){
 	if (typeof($.livequery) != 'function') return;
 	
 	// 1. Input datetime
+	var calendarloaded = null;
 	var dyndtwebroot = gdn.definition('WebRoot') + 'plugins/Morf/vendors/jquery.dynDateTime';
 	var input = document.createElement("input");
 	var inputs = {
 		"date": {ifFormat: "%Y-%m-%d", button: ".next()"},
 		"datetime": {ifFormat: "%Y-%m-%d %H:%M", showsTime: true, button: ".next()"}
 	};
-	var pumpinput = function(input, settings){
+	var pumpinput = function(element, settings){
+		if (element.type != "text") return;
 		// make sure that Calendar is loaded
-		if(typeof(Calendar) == 'function') settings.firstDay = Calendar._FD || 1;
-		if($(input).data('datetime') != null) return;
-		$(input).attr('autocomplete', 'off').data('datetime', true);
-		$('<button class="Button DateBoxPicker" title="Choose date...">...</button>').insertAfter(input);
-		$(document.createTextNode(" ")).insertAfter(input);
-		$(input).dynDateTime(settings);
+		if (typeof(Calendar) != 'function') throw new Error("Calendar not loaded.");
+		settings.firstDay = Calendar._FD || 1;
+		if($(element).data('datetime') != null) return;
+		$(element).attr('autocomplete', 'off').data('datetime', true);
+		$('<button class="Button DateBoxPicker" title="Choose date...">...</button>').insertAfter(element);
+		$(document.createTextNode(" ")).insertAfter(element);
+		$(element).dynDateTime(settings);
 	};
-	var setinputs = function(){
-		$.each(inputs, function(attribute, settings){
-			pumpinput("input[type='"+attribute+"']", settings);
-		});
-	};
-	var calendarloaded = null;
+	
 	$.each(inputs, function(attribute, settings){
 		$("input[type='"+attribute+"']").livequery(function(){
-			input.setAttribute("type", attribute);
-			if (input.type != "text") return;
+			var self = this;
+			if (calendarloaded) return pumpinput(self, settings);
 			if (calendarloaded === null) {
 				calendarloaded = false;
 				LazyLoad.css(dyndtwebroot+'/skins/aqua/theme.css');
 				var files = [dyndtwebroot+'/jquery.dynDateTime.pack.js', dyndtwebroot+'/lang/'+gdn.definition('CalendarLanguage')];
 				LazyLoad.js(files, function(){
 					calendarloaded = true;
-					setinputs();
+					pumpinput(self, settings);
 				});			
 			};
-			if (calendarloaded) pumpinput(this, settings);
+			
 		});
 	});
-	
 	
 	// 24 Mar 2011 upload box
 	var dot_progress = function(inputtextbox) {
@@ -84,12 +81,6 @@ $(document).ready(function(){
 				var percent = parseInt(event.loaded / event.total * 100, 10);
 				var stringprogress = file.name + ' / ' + parseInt(event.loaded/1024) + 'K ('+percent+'%)';
 				$(inputtextbox).val(stringprogress);
-				//console.log(file, 'event.loaded', event.loaded, 'event.total', event.total);
-				//console.log(event, files, index, xhr, handler);
-				//alert([event, files, index, xhr, handler]);
-/*				if (handler.progressbar) {
-					handler.progressbar.progressbar('value', parseInt(event.loaded / event.total * 100, 10));
-				}*/
 			}
 		});
 	}
@@ -98,11 +89,13 @@ $(document).ready(function(){
 	var uploadwebroot = gdn.definition('WebRoot') + 'plugins/Morf/vendors/blueimp-jquery-file-upload';
 	var receivefileurl = gdn.url('plugin/receiveuploadfile');
 	$('input[name$=UploadBoxFile]').livequery(function(){
-		var files = [];
+		var uploadboxfile = this, files = [];
 		files[files.length] = uploadwebroot+'/jquery.fileupload.js';
 		//files[files.length] = 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js';
 		files[files.length] = uploadwebroot+'/jquery.fileupload-ui.js';
-		LazyLoad.js(files, init_upload_input); // TODO: fix this
+		LazyLoad.js(files, function(){
+			init_upload_input.call(uploadboxfile);
+		});
 	});
 
 	// 25 mar 2011 (https://developer.mozilla.org/en/DOM/File)
